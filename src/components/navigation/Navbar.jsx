ab23f201181df8a470ff3463b981de751069783d
Carlito Algarin <carlito1113@yahoo.com>
2021-08-04 00:37:45 -0400

    starting layout, navbar and intro section

---
+++ src/components/navigation/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navbar.scss';
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+        <h2 className="navbar__logo">{`{C.A}`}</h2>
+        <div className="navbar__item">About</div>
+        <div className="navbar__item">Portfolio</div>
+        <div className="navbar__item">Contact</div>
+      </nav>
+    </div>
+  );
+}
