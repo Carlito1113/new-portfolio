aae8cf19318c046be3e087aead6d6a72caaaa952
Carlito Algarin <carlito1113@yahoo.com>
2021-08-05 16:26:35 -0400

    styled the works section and added links to the images

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -10,16 +10,74 @@ import expensetracker from '../../assets/img/expensetracker.png';
 export default function Works() {
   return (
     <div>
-      {/* <h1 className='h1test'>My Work</h1> */}
-
       <div className="work">
-        <h1 className='h1test2'>My Work</h1>
-        <img src={recipebook} alt="" className="work__image work__image--p1" />
-        <img src={recommend} alt="" className="work__image work__image--p2" />
-        <img src={audiophyle} alt="" className="work__image work__image--p3" />
-        <img src={wineanddines} alt="" className="work__image work__image--p4" />
-        <img src={fitnesstracker} alt="" className="work__image work__image--p5" />
-        <img src={expensetracker} alt="" className="work__image work__image--p6" />
+        <h1 className="work__header">My Work</h1>
+        <a
+          href="https://recipebook-2096c.web.app/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={recipebook}
+            alt="recipebook app"
+            className="work__image work__image--p1"
+          />
+        </a>
+        <a
+          href="https://recom-mend.herokuapp.com"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={recommend}
+            alt="recommend app"
+            className="work__image work__image--p2"
+          />
+        </a>
+        <a
+          href="https://audiophyle-music-platform.herokuapp.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={audiophyle}
+            alt="audiophyle app"
+            className="work__image work__image--p3"
+          />
+        </a>
+        <a
+          href="https://andrewrb22.github.io/-.theFederationOfCoders-/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={wineanddines}
+            alt="wine and dines app"
+            className="work__image work__image--p4"
+          />
+        </a>
+        <a
+          href="https://infinite-journey-49679.herokuapp.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={fitnesstracker}
+            alt="fitness tracker app"
+            className="work__image work__image--p5"
+          />
+        </a>
+        <a
+          href="https://damp-falls-85688.herokuapp.com/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            src={expensetracker}
+            alt="expense tracker app"
+            className="work__image work__image--p6"
+          />
+        </a>
       </div>
     </div>
   );
