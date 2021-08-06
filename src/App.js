633af47a186c067e638c292fa752e95d310f8e06
Carlito Algarin <carlito1113@yahoo.com>
2021-08-06 18:16:29 -0400

    used state to pass in props for the navigation so that it could change once either the nav or a link is clicked

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Navigation from './components/navigation/Navigation';
 import Intro from './components/intro/Intro';
 import Works from './components/works/Works';
 import Contact from './components/contact/Contact';
+import { useState } from 'react';
 
 function App() {
+  const [navOpen, setNavOpen] = useState(false);
   return (
     <div className="App">
       <Navbar />
-      <Navigation />
+      <Navigation navOpen={navOpen} setNavOpen={setNavOpen} />
       <div className="sections">
         <Intro id="intro" />
         <Works id="works" />
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,42 +1,38 @@
 import React from 'react';
 import './Navigation.scss';
 
-export default function Navigation() {
+export default function Navigation( {navOpen ,setNavOpen} ) {
   return (
     <div className="navigation">
-      <input
-        type="checkbox"
-        className="navigation__checkbox"
-        id="navi-toggle"
-      />
+      <input type="button" onClick={() => {setNavOpen(!navOpen)}} className="navigation__checkbox" id="nav-toggle" />
 
-      <label htmlFor="navi-toggle" className="navigation__button">
+      <label htmlFor="nav-toggle" className="navigation__button" id="nav-label">
         <span className="navigation__icon">&nbsp;</span>
       </label>
 
-      <div className="navigation__background">&nbsp;</div>
+      <div className={"navigation__background " + (navOpen && 'active') }>&nbsp;</div>
 
-      <nav className="navigation__nav">
+      <nav className={"navigation__nav " + (navOpen && 'active--nav')}>
         <ul className="navigation__list">
           <li className="navigation__item">
             <a href="#intro" className="navigation__link">
-              <label htmlFor="navi-toggle">
-                <span>Intro</span>
-              </label>
+              <button onClick={() => setNavOpen(!navOpen)} className="navigation__button--2">
+                Intro
+              </button>
             </a>
           </li>
           <li className="navigation__item">
             <a href="#portfolio" className="navigation__link">
-              <label htmlFor="navi-toggle">
-                <span>Portfolio</span>
-              </label>
+              <button onClick={() => setNavOpen(!navOpen)} className="navigation__button--2">
+              Portfolio
+              </button>
             </a>
           </li>
           <li className="navigation__item">
             <a href="#contacts" className="navigation__link">
-              <label htmlFor="navi-toggle">
-                <span>Contact</span>
-              </label>
+              <button onClick={() => setNavOpen(!navOpen)} className="navigation__button--2">
+                Contacts
+              </button>
             </a>
           </li>
         </ul>
