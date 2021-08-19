039182792f67a9c9aa770374b7379bb3f227b162
Carlito Algarin <carlito1113@yahoo.com>
2021-08-19 17:32:40 -0400

    created a contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 import './Contact.scss';
+import anniestellarcolor from '../../assets/img/annistellarcolor.jpg';
 
 export default function Contact() {
-  return <div className="contact" id="contacts"></div>;
+  return (
+    <div className="contact" id="contacts">
+      <div className="left">
+        <img src={anniestellarcolor} alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form>
+          <input type="text" placeholder="Email" />
+          <textarea></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
 }
