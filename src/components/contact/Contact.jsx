324ac35da79441912c29b676cba3f5af1ad7a1ea
Carlito Algarin <carlito1113@yahoo.com>
2021-08-19 19:57:41 -0400

    added in email functionality in my contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
+import { useState } from 'react';
 import './Contact.scss';
-import anniestellarcolor from '../../assets/img/annistellarcolor.jpg';
+import adrianblacksands from '../../assets/img/adrianblacksands.jpg';
+import emailjs from 'emailjs-com';
 
 export default function Contact() {
+  const [message, setMessage] = useState(false);
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_r0laelk', 'template_d932vm6', e.target, "user_y6VmyAoeST51goHoaPNmm")
+      .then(
+        result => {
+          console.log(result.text);
+        },
+        error => {
+          console.log(error.text);
+        }
+      );
+      e.target.reset()
+
+      setMessage(true);
+  }
+
   return (
     <div className="contact" id="contacts">
       <div className="left">
-        <img src={anniestellarcolor} alt="" />
+        <img src={adrianblacksands} alt="" />
       </div>
+
       <div className="right">
         <h2>Contact</h2>
-        <form>
-          <input type="text" placeholder="Email" />
-          <textarea></textarea>
+        <form onSubmit={sendEmail}>
+          <input type="email" placeholder="Email" name='email' />
+          <textarea placeholder='Your Message' name='message'></textarea>
           <button type="submit">Send</button>
+          {message && <span>Thanks, I will reply ASAP.</span>}
         </form>
       </div>
     </div>
