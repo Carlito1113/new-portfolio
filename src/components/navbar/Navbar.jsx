3664bacfc54a9725d20dca7945c4f87fc06625a6
Carlito Algarin <carlito1113@yahoo.com>
2021-08-19 20:14:07 -0400

    added in icons and some contact info into the navbar area

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import './Navbar.scss';
+import PersonIcon from '@material-ui/icons/Person';
+import MailIcon from '@material-ui/icons/Mail';
 
 export default function Navbar() {
   return (
-    <nav id="intro">
-      <div className="navbar">
+    <nav className='navbar' id="intro">
         <h2 className="navbar__logo">{`{C.A}`}</h2>
+      <div className="itemContainer">
+        <PersonIcon className="icon" />
+        <span>+727 331 0237</span>
       </div>
-    </nav> 
+      <div className="itemContainer">
+        <MailIcon className="icon"></MailIcon>
+        <span>carlito1113@yahoo.com</span>
+      </div>
+      <div className="itemContainer">
+        <a
+          href="https://github.com/Carlito1113"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fab fa-github icon"></i>
+        </a>
+      </div>
+      <div className="itemContainer">
+        <span>
+          <a
+            href="https://www.linkedin.com/in/carlito-algarin-5b29771b1/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in icon"></i>
+          </a>
+        </span>
+      </div>
+    </nav>
   );
 }
